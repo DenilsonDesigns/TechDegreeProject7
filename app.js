9004e99b717b349968715cd66c52308795918278
DenilsonDesigns <daniellanguilller@gmail.com>
2018-07-27 11:11:50 +1000

    async issues

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const config= require('./js/config');
 const Twit = require('twit');
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+
+
 
 //SETTING UP TWIT MODULE OPTIONS
 const T = new Twit({
@@ -13,16 +19,37 @@ const T = new Twit({
 const options = { screen_name: config.screen_name,
  count: 5 };
 
-//ITERATE THROUGH TWEETS AND PRINT TO CONSOLE
-T.get('statuses/user_timeline', options , function(err, data) {
-  for (var i = 0; i < data.length ; i++) {
-    console.log(data[i].text);
-  }
-});
+const tweetsSent = [];
 
+async function getTweets(){
+  try{
+    //ITERATE THROUGH TWEETS AND PRINT TO CONSOLE
+    await T.get('statuses/user_timeline', options , function(err, data) {
+      for (var i = 0; i < data.length ; i++) {
+        // console.log(data[i].text);
+        tweetsSent.push(data[i].text);
+        console.log(tweetsSent[i]);
+      }
+    });
+  }catch(err){
+    console.log('Could not retrieve tweets');
+  }
+}
+getTweets();
+
+app.get('/',(req,res)=>{
+  
+  res.render('index');
+});
 ////////////////
 ///SEND TWEET///
 ////////////////
 // T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
 //   console.log(data)
 // });
+
+//PORT
+const port = 3000;
+app.listen(port, ()=>{
+  console.log(`Listening on port ${port}`);
+});
